e1f01c09ca715c8726f6054249b3f9320dacea40
booldook <booldook@gmail.com>
2021-03-20 11:16:27 +0900

    ..

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 const express = require('express')
-const app = express();
+const app = express()
+const path = require('path')
+const createError = require('http-errors')
 
 app.listen(3000, () => {
 	console.log('=====================');
+	console.log('Server Start!');
 	console.log('http://127.0.0.1:3000');
 	console.log('=====================');
-});
-
-app.get('/', function(req, res){
-	const myName = 'booldook';
-	res.send('<h1>Hello '+myName+'</h1>');
 })
 
-app.get('/hello', function(req, res){
-	res.send('<h1>Hello, Express</h1>')
+app.use('/', express.static(path.join(__dirname, './public')))
+app.use('/uploads', express.static(path.join(__dirname, './storages')))
+
+/* Middleware */
+app.use((req, res, next) => {
+	req.nowTime = new Date()
+	next()
 })
 
-app.get('/api', (req, res) => {
-	const id = req.query.id;
-	const users = [
-		{id: 1, name: '홍길동', kor: 75},
-		{id: 2, name: '홍길만', kor: 80},
-		{id: 3, name: '홍길순', kor: 85},
-	]
-	var sendUser = id ? users.filter(v => id == v.id) : [...users];
-	res.json(sendUser);
+app.get('/', (req, res, next) => {
+	res.send('<h1>Hello Root'+req.nowTime+'</h1>')
+})
+
+app.get('/hello', (req, res, next) => {
+	next(createError(500, {msg: 'DB에러가 발생하였습니다.'}))
+	//res.send('hello'+req.nowTime)
+})
+
+app.use((req, res, next) => {
+	const msg = '<h1 style="margin: 100px;">Error 404</h1><div>'+req.nowTime+'</div>';
+	next(createError(404, {code: 404, msg}));
+})
+
+app.use((err, req, res, next) => {
+	console.log(err)
+	res.send(err.msg)
 })
---
+++ app_01.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+
+app.listen(3000, () => {
+	console.log('=====================');
+	console.log('http://127.0.0.1:3000');
+	console.log('=====================');
+});
+
+app.get('/', function(req, res){
+	const myName = 'booldook';
+	res.send('<h1>Hello '+myName+'</h1>');
+})
+
+app.get('/api2/:name', function(req, res){
+	const myName = req.params.name || 'booldook';
+	res.send('<h1>Hello '+myName+'</h1>');
+})
+
+app.get('/hello', function(req, res){
+	res.send('<h1>Hello, Express</h1>')
+})
+
+app.get('/api', (req, res) => {
+	const id = req.query.id;
+	const users = [
+		{id: 1, name: '홍길동', kor: 75},
+		{id: 2, name: '홍길만', kor: 80},
+		{id: 3, name: '홍길순', kor: 85},
+	]
+	var sendUser = id ? users.filter(v => id == v.id) : [...users];
+	res.json(sendUser);
+})
